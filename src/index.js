44762714f97e73e86055152bb707f2bbecce8713
[your <leigh.tm@gmail.com>
2019-05-16 00:42:25 -0400

    still working on it

--- src/index.js
+++ src/index.js
@@ -3,19 +3,29 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware , compose } from "redux";
 import rootReducer from "./reducers";
+import { CharacterList } from "./components";
+import thunk from 'redux-thunk';
+
+
 // needed dependancies
 // applyMiddleware from redux
 // thunk from redux-thunk
 // logger from redux-logger
 // rootReducer from ./reducers
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-  rootReducer
-  /* applyMiddleware goes here */
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
 
+
+
+
+const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
     <App />
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,38 @@
+import React  from 'react';
+import { connect } from 'react-redux';
 import { combineReducers } from 'redux';
 import { charsReducer } from './starWarsReducer';
+
+
+const CharacterList = props => {
+  const fetchPhoto = e => {
+    e.preventDefault();
+    props.getPhoto();
+  };
+
+  return (
+    <div>
+    <h2> StarWar and Beyound</h2>
+    {props.photoOfTheDay && (
+      <div>
+        <h4>{props.photoOfTheDay.title}</h4>
+        <img src={props.photoOfTheDay.url} alt="NASA Photo of the day" />
+        <p>{props.photoOfTheDay.explanation}</p>
+      </div>
+    )}
+    {props.isLoading && (
+      <Loader type="Ball-Triangle" color="#00BFFF" height="90" width="60" />
+    )}
+    {props.error && <p className="error">{props.error}</p>}
+    <Button color="info" onClick={fetchPhoto}>
+      🛰 See today's photo 📸
+    </Button>
+  </div>
+);
+};
+  
+
+
 
 export default combineReducers({
   charsReducer
